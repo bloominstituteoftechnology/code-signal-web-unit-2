e107f005aab8788a89ffdbbf73ff043b0cf81af8
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-30 18:19:10 +0200

    wip

--- index.test.js
+++ index.test.js
@@ -37,14 +37,44 @@ describe('Unit 2:1 - Applied JavaScript', () => {
 /*
 Unit 2:2 - Intro to React
 
-Write a function called "divMaker".
-It takes an "attributes" object as the first argument,
-and a string "textContent" as the second argument.
-It returns a "div" element with the correct attributes and text content.
+1- Write a React component called "LightSwitch".
 
-Example of usage:
+It receives a single prop called "on".
+If "on" has a value of true, the component returns a "p" tag with the text "The switch is on!".
+If "on" has a value of false, the component returns a "p" tag with the text "The switch is off".
 
-const div = divMaker({ id: 'greet' }, 'Hello, World')
-console.log(div) // <div id="greet">Hello, World</div>
+2- Write a second React component called "Switches".
+
+It expects a "number" prop which is a number.
+It returns one "div" tag containing "n" LightSwitches, where "n" is equal to the value of "number".
+If "n" is an even number, all LightSwitches are on. Otherwise all LightSwitches are off.
 
 */
+
+function LightSwitch({ on }) {
+  return <p>{on ? 'The switch is on!' : 'The switch is off'}</p>
+}
+function Switches({ number }) {
+  const switches = Array.from(Array(number).keys())
+  return <div>{switches.map((_, idx) => <LightSwitch key={idx} on={number % 2 === 0} />)}</div>
+}
+
+describe('Unit 2:2 - Intro to React', () => {
+  test('Switches works with even number', () => {
+    const testRenderer = TestRenderer.create(<Switches number={4} />).toJSON()
+
+    testRenderer.children[0].children.forEach(text => {
+      expect(text).toMatch(/on/i)
+    })
+    expect(testRenderer.children).toHaveLength(4)
+  })
+
+  test('Switches works with odd number', () => {
+    const testRenderer = TestRenderer.create(<Switches number={3} />).toJSON()
+
+    expect(testRenderer.children).toHaveLength(3)
+    testRenderer.children[0].children.forEach(text => {
+      expect(text).toMatch(/off/i)
+    })
+  })
+})
